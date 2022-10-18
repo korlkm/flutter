a4d49017e61c14bc06f1f0b2d03f45782ca5fec4
korlkm <lkm060825@dgsw.hs.kr>
2022-10-18 18:16:02 +0900

    버튼 누르면 카운트하는 리스트뷰

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+
+class MyApp extends StatefulWidget{
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 0;
+  var name = ['김명준', '이찬혁', '김치찜'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
@@ -17,8 +29,13 @@ class MyApp extends StatelessWidget {
           itemCount: 3,
           itemBuilder: (context, i){
             return ListTile(
-              leading: Image.asset('koko.jpeg'),
-              title: Text('홍길동'),
+              leading: Text(like[i].toString()),
+              title: Text(name[i]),
+              trailing: ElevatedButton(child: Text('좋아요'), onPressed: (){
+                setState(() {
+                  like[i]++;
+                });
+              }),
             );
           },
         )
